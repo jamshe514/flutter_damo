948095d5d78ee3d93bde91b2f2a2db03bf4ad274
jamshe514 <jamshee514@gamil.com>
2024-02-27 13:21:45 +0530

    change

--- bin/quotienreminder.dart
+++ bin/quotienreminder.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
 void reminder(){
-  print("enter the no:");
+  print("enter the no  :");
   int  a=int.parse(stdin.readLineSync()!);
   int  b=int.parse(stdin.readLineSync()!);
   int re=a%b;
